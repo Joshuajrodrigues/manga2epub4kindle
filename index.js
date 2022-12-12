3bc8b5b7dbaa864afd8975e7750cd11ad120c374
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2022-12-12 22:16:55 +0530

    major changes to async

--- index.js
+++ index.js
@@ -13,57 +13,57 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 let metaDataAnswers = {
   series: "",
   author: "",
 };
 
-const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms))
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
 const welcome = async () => {
   const intro = chalkAnimation.rainbow("Welcome to Mangapub 🍺");
   await sleep();
   intro.stop();
   console.log(`
-    ${chalk.redBright('Make sure you ran me in a folder full of cbr/cbz files.')}
-  `)
+    ${chalk.redBright(
+      "Make sure you ran me in a folder full of cbr/cbz files."
+    )}
+  `);
 };
 
 const processImage = async (path, filename) => {
   console.log(chalk.greenBright("Processing images..."));
   let dir = join(__dirname, path);
   let files = readdirSync(dir);
+//  console.log("batch",files.length)
   for (let i in files) {
-    let file = files[i]
+    let file = files[i];
     await sharp(join(dir, file))
-      .trim()
       .metadata()
       .then(async ({ width, height }) => {
         // width > height
         if (width > height) {
-          sharp(join(dir, file))
+          await sharp(join(dir, file))
             // divide into 2 parts 0 to width/2 and width/2 to width
             .extract({ width: width / 2, height, left: 0, top: 0 })
             //add these 2 images instead of the original
             .toFile(`./extracted/${filename}/${file}`.replace(".png", "-2.png"))
-            .then(() => {
-              sharp(join(dir, file))
+            .then(async() => {
+              await sharp(join(dir, file))
                 .extract({ width: width / 2, height, left: width / 2, top: 0 })
                 .toFile(
                   `./extracted/${filename}/${file}`.replace(".png", "-1.png")
                 )
-                .then(() => {
+                .then(async() => {
                   //remove original
-                  files.splice(i, 1)
+                 // console.log("removing",file)
+                  files.splice(i, 1);
                   unlink(`./extracted/${filename}/${file}`, (err) => {
                     if (err) throw err;
-
                   });
                 });
             });
         }
-
       })
     // .then(() => {
     //   console.log("done processing")
@@ -72,23 +72,18 @@ const processImage = async (path, filename) => {
   }
 
   for (let i in files) {
-    let file = files[i]
-    let trimmed = await sharp(join(dir, file))
-      .trim()
-      .toBuffer();
-    await sharp(trimmed)
-      .toFile(`./extracted/${filename}/${file}`)
+    let file = files[i];
+    //console.log("file",file)
+    let trimmed = await sharp(join(dir, file)).trim().jpeg().toBuffer();
+    await sharp(trimmed).toFile(`./extracted/${filename}/${file}`);
     // .then(() => {
     //   console.log("done trimming", file)
 
     // })
   }
 
-
-  readExtracted()
-
+  
 };
-
 
 const convertToEpub = async (filename, index) => {
   console.log(chalk.greenBright("Converting to Epub..."));
@@ -98,6 +93,7 @@ const convertToEpub = async (filename, index) => {
   let cover = join(dir, files[0]);
   let images = files.map((file) => join(dir, file));
   files.shift();
+
   var metadata = {
     id: new Date(),
     cover: cover,
@@ -111,30 +107,52 @@ const convertToEpub = async (filename, index) => {
     language: "en",
     showContents: false,
     images: images,
+
+    pageDirection:'rtl',
+    originalResWidth:1072,
+    originalResHeight:1448,
+    kindleComicConverter:true
   };
 
   let epub = nodepub.document(metadata);
   files.forEach((image, index) => {
     epub.addSection(
       `Page ${index}`,
-      `<img width="100vw" height="100vh" src='../images/${image}' />`
+      `
+      <div style="text-align:center;top:0.0%;">
+      <img width="1072" height="1448" src='../images/${image}' />
+      </div>
+      `
     );
+    epub.addCSS(`
+    @page {
+      margin: 0;
+      }
+    body {
+        display: block;
+        margin: 0;
+        padding: 0;
+        }
+        `);
   });
-  await epub.writeEPUB("./epub", filename.replace(".zip", '').replace(".cbz", '')).then(() => {
-    rmSync(`./extracted/${filename}`, { recursive: true });
-  });
+  await epub
+    .writeEPUB("./epub", filename.replace(".zip", "").replace(".cbz", ""))
+    .then(() => {
+      rmSync(`./extracted/${filename}`, { recursive: true });
+    });
 };
 
 const unziper = async (filePath, filename, index) => {
   console.log(chalk.greenBright("Extracting images..."));
   createReadStream(filePath)
-    .pipe(unzipper.Extract({
-      path: `./extracted/${filename}`,
-    }))
-    .on('close', async () => {
+    .pipe(
+      unzipper.Extract({
+        path: `./extracted/${filename}`,
+      })
+    )
+    .on("close", async () => {
       await processImage(`./extracted/${filename}`, filename);
-
-    })
+    });
 };
 
 const metaDataQuestions = async () => {
@@ -143,32 +161,32 @@ const metaDataQuestions = async () => {
     type: "input",
     message: "Enter series name",
     default() {
-      return ''
-    }
+      return "";
+    },
   });
   let q2 = await inquirer.prompt({
     name: "author",
     type: "input",
     message: "Enter author name",
     default() {
-      return ''
-    }
+      return "";
+    },
   });
   let q3 = await inquirer.prompt({
     name: "genre",
     type: "input",
     message: "Enter Genre name",
     default() {
-      return ''
-    }
+      return "";
+    },
   });
   let q4 = await inquirer.prompt({
     name: "publisher",
     type: "input",
     message: "Enter publisher name",
     default() {
-      return ''
-    }
+      return "";
+    },
   });
   let q5 = await inquirer.prompt({
     name: "kindle",
@@ -176,8 +194,8 @@ const metaDataQuestions = async () => {
     message: "Select your kindle",
     choices: ["Paperwhite 11th gen", "Basic Kindle"],
     default() {
-      return "Paperwhite 11th gen"
-    }
+      return "Paperwhite 11th gen";
+    },
   });
   // select kindle
   // add genre
@@ -192,21 +210,20 @@ const readDirectory = async () => {
   let dir = join(__dirname, "./testDir");
   let files = readdirSync(dir);
   files.map(async (file, index) => {
-    await unziper(join(dir, file), file, index)
+    await unziper(join(dir, file), file, index);
   });
+  
 };
 
 const readExtracted = async () => {
-
   let dir = join(__dirname, "./extracted");
   let files = readdirSync(dir);
   files.map(async (file, index) => {
-    convertToEpub(file, index)
+    convertToEpub(file, index);
   });
-  console.log("done")
+  console.log("done");
 };
-
-
 
 await welcome();
 await readDirectory();
+await readExtracted();
